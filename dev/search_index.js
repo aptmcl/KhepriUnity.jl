bd1c5b02edcee9450fb9b392ada5e265d1956a34
Documenter.jl <documenter@juliadocs.github.io>
2024-09-30 09:09:54 +0000

    build based on d0451cb

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KhepriUnity","category":"page"},{"location":"#KhepriUnity","page":"Home","title":"KhepriUnity","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KhepriUnity]","category":"page"}]
+}
